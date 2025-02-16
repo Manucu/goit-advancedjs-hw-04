335a7e52ef1d09d218bf04d43d3af98de33bbd49
Emanuel Niculai <65870921+Manucu@users.noreply.github.com>
2025-02-16 22:22:27 +0000

    final commit

--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import iziToast from 'izitoast';
 import SimpleLightbox from 'simplelightbox';
 
 import {
-  renderSearchFormElement,
-  renderImagesElement,
-  renderGallery, renderLoadingElement,
+  renderSearchForm,
+  renderNewImages,
+  renderGalleryWrapper,
+  renderLoader,
+  renderButton, renderImages,
 } from './js/render-functions.js';
 import { getImagesFromAPI } from './js/pixabay-api.js';
 
@@ -12,29 +14,65 @@ import 'izitoast/dist/css/iziToast.min.css';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
 iziToast.settings({
-  position: 'topRight',
   timeout: 3000,
 });
 
-const onSubmitSearchFormHandler = (event, form, galleryContainer, gallery) => {
+const gallery = new SimpleLightbox('#gallery a', {
+  captionDelay: 250,
+  captionsData: 'alt',
+});
+
+const images = [];
+let query = '';
+let page = 1;
+
+const searchResultsWarning = () => {
+  iziToast.warning({
+    icon: '',
+    iconText: '',
+    title: '⚠️ Warning',
+    message: "We're sorry, but you've reached the end of search results.",
+  });
+}
+
+const resetGallery = () => {
+  images.length = 0;
+  page = 1;
+}
+
+const onSubmitSearchFormHandler = async (event, form) => {
   event.preventDefault();
-  const query = form.elements.search.value;
+  query = form.elements.search.value;
+  const loader = document.querySelector('.loader-wrapper');
 
   if (query.trim() !== '') {
-    renderLoadingElement(galleryContainer);
+    const galleryContainer = document.querySelector('#gallery');
+    loader.classList.remove('hidden');
+    resetGallery();
 
-    getImagesFromAPI(query).then((images) => {
-      renderImagesElement(galleryContainer, images);
-      gallery.refresh();
-      if (!images.length) {
-        iziToast.error({
-          icon: '',
-          iconText: '',
-          title: '❌ Error',
-          message: 'Sorry, there are no images matching your search query. Please try again!',
-        });
+    const { images: newImages, total } = await getImagesFromAPI(query, page);
+
+    renderNewImages(galleryContainer, newImages);
+
+    if (newImages.length) {
+      const button = document.querySelector('.load-more');
+      images.push(...newImages);
+
+      if (newImages.length >= total) {
+        searchResultsWarning();
+        button.classList.add('hidden');
+      } else {
+        button.classList.remove('hidden');
       }
-    });
+      gallery.refresh();
+    } else {
+      iziToast.error({
+        icon: '',
+        iconText: '',
+        title: '❌ Error',
+        message: 'Sorry, there are no images matching your search query. Please try again!',
+      });
+    }
   } else {
     iziToast.warning({
       icon: '',
@@ -44,24 +82,60 @@ const onSubmitSearchFormHandler = (event, form, galleryContainer, gallery) => {
     });
   }
 
+  loader.classList.add('hidden');
   form.reset();
+};
+
+const onLoadImagesHandler = async (button) => {
+  const loader = document.querySelector('.loader-wrapper');
+  const galleryContainer = document.querySelector('#gallery');
+
+  button.classList.add('hidden');
+  loader.classList.remove('hidden');
+
+  const { images: newImages, total } = await getImagesFromAPI(query, page += 1);
+  images.push(...newImages);
+
+  renderImages(galleryContainer, newImages);
+  gallery.refresh();
+
+  if (images.length >= total || newImages.length === 0) {
+    searchResultsWarning();
+  } else {
+    button.classList.remove('hidden');
+  }
+
+  if (newImages.length !== 0) {
+    const image = document.querySelector('.gallery-item');
+    if (image) {
+      const height = image.getBoundingClientRect().height;
+      window.scrollBy({
+        top: height * 2,
+        behavior: 'smooth',
+      });
+    }
+  }
+
+  loader.classList.add('hidden');
 };
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
 
-  renderSearchFormElement(root);
-  renderGallery(root);
+  renderSearchForm(root);
+  renderGalleryWrapper(root);
+  renderLoader(root);
+  renderButton(root);
 
   const form = document.querySelector('#search-form');
-  const galleryContainer = document.querySelector('#gallery');
 
-  const gallery = new SimpleLightbox('#gallery a', {
-    captionDelay: 250,
-    captionsData: 'alt',
+  form.addEventListener('submit', async (event) => {
+    await onSubmitSearchFormHandler(event, form);
   });
 
-  form.addEventListener('submit', (event) => {
-    onSubmitSearchFormHandler(event, form, galleryContainer, gallery);
+  const button = document.querySelector('.load-more');
+
+  button.addEventListener('click', async () => {
+    await onLoadImagesHandler(button);
   });
 });
